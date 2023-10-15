9377d4453a0cedb4965027ec2ebfbf27baf8de2f
makst <maks.tierientiev00@gmail.com>
2023-10-16 00:20:14 +0300

    start project

---
+++ src/main/java/org/example/Melody.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Melody {
+    HashMap<Clip, long[]> map = new HashMap<>();
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/MelodyTest.java
@@ -0,0 +1,10 @@
+import org.example.Melody;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MelodyTest {
+    Melody melody = new Melody();
+
+}
